1f298c73aedb34722532309d49b17478fd7d4feb
gaurondorian <gaurondorian@gmail.com>
2021-12-11 16:47:23 +0100

    drapeau par svg et non icon, trie cas avec fleche et nb pays affiché

--- tableur.js
+++ tableur.js
@@ -1,9 +1,23 @@
 const divContainer = document.querySelector('.app')
 
-const getFlagEmoji = countryCode =>{
-    const a = [...countryCode.toUpperCase()].map(letter => 127397 + letter.charCodeAt() )
-    return String.fromCodePoint(a[0],a[1])
+const arrows = document.querySelectorAll('.arrow')
+
+let countries = []
+
+String.prototype.splitAtThree = function(){
+    return this.split("").reverse()
+    .map((letter,index)=> (index % 3 === 0) && (index != 0) ? letter+" " : letter)
+    .reverse().join("")
 }
+
+/* 
+Ne marche pas sur tout les navigateurs
+    const getFlagEmoji = countryCode =>{
+        const a = [...countryCode.toUpperCase()].map(letter => 127397 + letter.charCodeAt() )
+        return String.fromCodePoint(a[0],a[1])
+} */
+
+const getFLagIcon = countryCode => `https://flagcdn.com/${countryCode.toLowerCase()}.svg`
 
 const pibRequest = async (countries) =>{
 
@@ -18,7 +32,6 @@ const pibRequest = async (countries) =>{
     })
 }
 
-
 const initTable = async () =>{
 
     const result = await Request.send("https://api.covid19api.com/summary","GET")
@@ -28,13 +41,45 @@ const initTable = async () =>{
             nomDuPays : country.Country,
             codePaysISO2 : country.CountryCode,
             codePaysISO3 : dataCountry[country.CountryCode],
-            flag : getFlagEmoji(country.CountryCode),
-            totalDeCas : country.TotalConfirmed,
-            totalDeMort : country.TotalDeaths,
+            flag : getFLagIcon(country.CountryCode),
+            //flag : getFlagEmoji(country.CountryCode),
+            totalDeCas : country.TotalConfirmed.toString().splitAtThree(),
+            totalDeMort : country.TotalDeaths.toString().splitAtThree(),
             pib : 0
         }
     })
 
+}
+
+const updateTable = (countries) =>{
+
+    const table = document.querySelector(".body-table")
+
+    const trs = table.children
+    let jndex = 0
+
+    for (let index = 1; index < trs.length; index++) {
+        const element = trs.item(index)
+
+        const country = countries[jndex]
+
+        const ths = [...element.children]
+
+        ths.forEach( (el,index) =>{
+            if(index === 0)
+                el.children.item(0).src = country.flag
+            else if (index === 1)
+                el.textContent = country.nomDuPays
+            else if (index === 2)
+                el.textContent = country.totalDeCas
+            else if (index === 3)
+                el.textContent= country.totalDeMort
+            else
+                el.textContent = country.pib == 0 ? "Introuvable" : country.pib
+        })
+
+        jndex++
+    }
 }
 
 const createTable = (countries) =>{
@@ -48,8 +93,13 @@ const createTable = (countries) =>{
         for (let index = 0; index < 5; index++) {
             const th = document.createElement("th")
 
-            if(index === 0)
-                th.innerHTML = element.flag
+            if(index === 0){
+                //Pour les icones th.innerHTML = element.flag
+                const img = document.createElement('img')
+                img.setAttribute('class','flag-svg')
+                img.src = element.flag
+                th.appendChild(img)
+            }
             else if (index === 1)
                 th.innerHTML = element.nomDuPays
             else if (index === 2)
@@ -68,19 +118,78 @@ const createTable = (countries) =>{
     })
 }
 
+const changeArrow = (element) =>{
+    const flag = element.src.includes("tri1")
+
+    if (flag){
+        element.src = element.src.replace("tri1","tri2")
+        element.title = "Croissant"
+    }
+    else{
+        element.src = element.src.replace("tri2","tri1")
+        element.title = "Décroissant"
+    }
+
+    return element.title
+
+}
+
+const croissant = (countries, parameter) =>{
+    return countries.sort( (a,b)=> parseInt(a[parameter].replace(/\s+/g, '')) - parseInt(b[parameter].replace(/\s+/g, '')) )
+}
+
+const decroissant = (countries, parameter) =>{
+    return countries.sort( (a,b)=> parseInt(a[parameter].replace(/\s+/g, '')) - parseInt(b[parameter].replace(/\s+/g, '')) )
+    .reverse()
+}
 
 const main = async () =>{
-    let countries = await initTable()
+    countries = await initTable()
     await pibRequest(countries)
     createTable(countries)
 }
 
 
-document.querySelector("button").addEventListener('click',(e)=>{
-    console.log(e)
-    createTable
+arrows.forEach( (el, index) =>{
+    el.addEventListener('click', () => {
+
+        const trie = changeArrow(el)
+
+        switch(index){
+            case 1:{
+                countries = trie === "Décroissant" ? decroissant(countries,"totalDeCas") : croissant(countries,"totalDeCas")
+            }
+        }
+
+        console.log(countries)
+
+        updateTable(countries)
+    })
+})
+
+document.querySelector('button').addEventListener('click',(e)=>{
+
+    const input = document.querySelector("input").value
+    const table = document.querySelector(".body-table")
+
+    const childrens = table.children
+    if (input === ""){
+        for (let index = 0; index < childrens.length; index++) {
+            const element = childrens[index];
+            element.style.display = "table-row"
+        }
+    }
+
+    if ( !isNaN( parseInt(input) )){
+       for (let index = parseInt(input) + 1; index < childrens.length; index++) {
+           const element = childrens[index];
+           element.style.display = "none"
+       }
+    }
 })
 
 main()
 
+
 //TOOD Ajouter l'unité du PIB
+//Revoir le trie
